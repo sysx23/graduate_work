edfbc600b056e2964bf60b6cdabbd8b839c51731
Rostyslav Ivanika <Rostyslav.Ivanika@gmail.com>
2019-06-28 20:36:08 +0300

    add tags and security groups

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ variable "region" {
 	default = "eu-central-1"
 }
 
+variable "ami_id" {
+	type = string
+	default = "ami-0cc293023f983ed53"
+}
+
 variable "vpc_cidr" {
 	type = string
 	default = "172.16.0.0/16"
@@ -19,14 +24,59 @@ provider "aws" {
 	region = var.region
 }
 
-resource "aws_vpc" "graduate-work-vpc" {
+locals {
+	common_tags = {
+		Project = "graduate_work"
+	}
+}
+
+resource "aws_vpc" "graduate_work_vpc" {
 	cidr_block = "${var.vpc_cidr}"
+	tags = local.common_tags
 }
 
-resource "aws_subnet" "graduate-work-sn-a" {
+resource "aws_internet_gateway" "igw" {
+	vpc_id = aws_vpc.graduate_work_vpc.id
+	tags = local.common_tags
+}
+
+resource "aws_subnet" "default_sn" {
 	count = "1"
-	vpc_id = aws_vpc.graduate-work-vpc.id
-	cidr_block = "${cidrsubnet(aws_vpc.graduate-work-vpc.cidr_block,8,count.index)}"
+	vpc_id = aws_vpc.graduate_work_vpc.id
+	cidr_block = "${cidrsubnet(aws_vpc.graduate_work_vpc.cidr_block,8,count.index)}"
 	availability_zone = "${lookup(var.vpc_subnets_availability_zones, count.index)}"
+	tags = local.common_tags
 }
 
+resource "aws_security_group" "allow_ssh" {
+	vpc_id = aws_vpc.graduate_work_vpc.id
+	name = "allow ssh"
+	ingress {
+		cidr_blocks = ["0.0.0.0/0"]
+		protocol = "tcp"
+		from_port = "22"
+		to_port = "22"
+	}
+}
+
+resource "aws_security_group" "allow_http" {
+	vpc_id = aws_vpc.graduate_work_vpc.id
+	name = "allow http"
+	ingress {
+		cidr_blocks = ["0.0.0.0/0"]
+		protocol = "tcp"
+		from_port = "80"
+		to_port = "80"
+	}
+}
+
+resource "aws_security_group" "allow_all_outgoing" {
+	vpc_id = aws_vpc.graduate_work_vpc.id
+	name = "allow all outgoing"
+	egress {
+		cidr_blocks = ["0.0.0.0/0"]
+		protocol = "-1"
+		from_port = "0"
+		to_port = "0"
+	}
+}
